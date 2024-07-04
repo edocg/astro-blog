dc3f5d632d21b09c0873f6505ce74a4c8f222261
Jaimedoc <jaimecortes@elpoli.edu.co>
2024-07-03 21:29:03 -0500

    First commit

---
+++ src/content/config.ts
@@ -0,0 +1,41 @@
+import { defineCollection, reference, z } from "astro:content";
+
+
+
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: ({image})=> z.object({
+        title: z.string(),
+        date: z.date(),
+        description:z.string(),
+        image: image().refine(img => img.width < 1200, {
+            message:'Image Should be lower than 1200px'
+        }),
+        
+        //Relación
+        //author: z.string(),
+        author: reference('author'),
+        //RElación
+        tags: z.array(z.string()),
+        isDraft: z.boolean().default(false),
+    }),
+});
+
+
+
+const authorCollection = defineCollection({
+    type: 'data',
+    schema: ({image})=> z.object({
+        name: z.string(),
+        avatar: image(),
+        twitter: z.string(),
+        linkedIn: z.string(),
+        github: z.string(),
+        bio: z.string(),
+        subtitle: z.string(),
+    }),
+});
+export const collections = {
+    blog: blogCollection,
+    author: authorCollection,
+}
